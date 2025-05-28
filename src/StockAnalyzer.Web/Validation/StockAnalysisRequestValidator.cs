7d20b412c827723fda9f6be310b8435c30904ca1
joaoveasey <jovepedro@gmail.com>
2025-05-28 19:33:51 -0300

    feat: adicionando mais validações e lógica para envio de dados para o backend

--- src/StockAnalyzer.Web/Validation/StockAnalysisRequestValidator.cs
+++ src/StockAnalyzer.Web/Validation/StockAnalysisRequestValidator.cs
@@ -15,9 +15,33 @@ public class StockAnalysisRequestValidator
             errors.Add("O campo Ticket deve ter no máximo 6 caracteres.");
 
         if (string.IsNullOrWhiteSpace(stock.CurrentPrice))
-            errors.Add("O campo Cotação Atual deve estar preenhido.");
+            errors.Add("O campo Cotação Atual deve estar preenchido.");
+        //else if (int.Parse(stock.CurrentPrice) < 0)
+        //    errors.Add("O campo Cotação Atual deve ser um valor positivo.");
 
-        // outras validações
+        if (string.IsNullOrWhiteSpace(stock.TwelveMonthChange))
+            errors.Add("O campo Variação 12 Meses deve estar preenchido.");
+
+        if (string.IsNullOrWhiteSpace(stock.PriceToEarnings))
+            errors.Add("O campo Preço/Lucro (P/L) deve estar preenchido.");
+
+        if (string.IsNullOrWhiteSpace(stock.PriceToBook))
+            errors.Add("O campo Preço/Valor Patrimonial (P/VP) deve estar preenchido.");
+
+        if (string.IsNullOrWhiteSpace(stock.DividendYield))
+            errors.Add("O campo Dividend Yield deve estar preenchido.");
+
+        if (string.IsNullOrWhiteSpace(stock.ReturnOnEquity))
+            errors.Add("O campo ROE deve estar preenchido.");
+
+        if (string.IsNullOrWhiteSpace(stock.NetMargin))
+            errors.Add("O campo Margem Líquida deve estar preenchido.");
+
+        if (string.IsNullOrWhiteSpace(stock.EarningsPerShare))
+            errors.Add("O campo Lucro por Ação (LPA) deve estar preenchido.");
+
+        if (string.IsNullOrWhiteSpace(stock.Sector))
+            errors.Add("O campo Setor deve estar preenchido.");
 
         return errors;
     }
